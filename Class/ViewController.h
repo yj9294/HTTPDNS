b067ea48c19bcc4348f926117a464b6c18b35dcd
jack <497148400@qq.com>
2018-02-07 17:06:17 +0800

    no message

---
+++ Class/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKEmoj
+//
+//  Created by cptech on 2018/2/7.
+//  Copyright © 2018年 links123_yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Class/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  LKEmoj
+//
+//  Created by cptech on 2018/2/7.
+//  Copyright © 2018年 links123_yj. All rights reserved.
+//
+
+#import "ViewController.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
